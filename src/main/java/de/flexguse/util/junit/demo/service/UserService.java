e8d8f266f4cdd04b0046d081c34dc9b00b7df8fe
Christoph Guse <info@flexguse.de>
2015-03-29 14:51:02 +0200

    updated javadoc

--- src/main/java/de/flexguse/util/junit/demo/service/UserService.java
+++ src/main/java/de/flexguse/util/junit/demo/service/UserService.java
@@ -10,12 +10,19 @@ import org.springframework.validation.annotation.Validated;
 import de.flexguse.util.junit.demo.model.User;
 
 /**
+ * 
  * @author Christoph Guse, info@flexguse.de
  *
  */
 @Validated
 public interface UserService {
 
+	/**
+	 * Persists the user. The user always is validated when calling this method.
+	 * 
+	 * @param user
+	 *            the user which is to be persisted and validated
+	 */
 	public void saveUser(@Valid User user);
-	
+
 }
